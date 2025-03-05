1c5c61a01c5e6191a3da96e7ca762486d66f2a04
seondays <agseon9@gmail.com>
2025-03-05 10:12:23 +0900

    [chore] 스프링부트 초기 세팅

---
+++ src/main/java/com/seondays/boardLog/BoardLogApplication.java
@@ -0,0 +1,13 @@
+package com.seondays.boardLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seondays/boardLog/BoardLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seondays.boardLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
